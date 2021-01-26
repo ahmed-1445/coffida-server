69c32897897bdbbfe25d8c3d9e58a9b6e49694f9
Ahmed Malik <17032837@stu.mmu.ac.uk>
2021-01-26 17:17:58 +0000

    Created Login.js for Login screen.

---
+++ newApp/components/Login.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+// Login component
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleEmailInput = (email) => {
+    this.setState({email: email});
+  };
+
+  handlePasswordInput = (pass) => {
+    this.setState({password: pass});
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Label}>Email:</Text>
+        <TextInput
+          placeholder="Enter Email"
+          style={styles.Input}
+          onChangeText={this.handleEmailInput}
+          value={this.state.email}
+        />
+        <Text style={styles.Label}>Password:</Text>
+        <TextInput
+          placeholder="Enter Password"
+          style={styles.Input}
+          onChangeText={this.handlePasswordInput}
+          value={this.state.password}
+        />
+        <View style={styles.space} />
+        <TouchableOpacity style={styles.Touch} onPress={() => this.signUp()}>
+          <Text style={styles.TouchText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 2,
+    backgroundColor: 'lightseagreen',
+  },
+  Label: {
+    fontSize: 13,
+    color: 'white',
+  },
+  Input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+  },
+  Touch: {
+    backgroundColor: 'darkorchid',
+    padding: 10,
+    alignItems: 'center',
+  },
+  TouchText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+});
+
+export default Login;
